247316e9adaf6a242289b0aea9aee57996ee9fdf
Crisbel-Enobay <91605851+Crisbel-Enobay@users.noreply.github.com>
2024-01-18 21:45:57 +0800

    update laravel

--- laravel-app/resources/views/product/index.blade.php
+++ laravel-app/resources/views/product/index.blade.php
@@ -10,59 +10,46 @@
 </head>
 
 <body class="bg-dark-subtle">
-    <main>
-
-        <div class="album py-5">
-            <div class="container">
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-md-4 g-3">
-                    @forelse ($products as $product)
-                        <div class="col">
-                            <div class="card shadow-sm">
-                                <img src="{{ asset('storage/' . $product->image_path) }}" alt="" width="100%"
-                                    height="225" data-bs-toggle="modal" data-bs-target="#myProduct{{ $product->id }}"
-                                    data-product-id="{{ $product->id }}">
-                                <div class="card-body">
-                                    <p class="card-text">{{ $product->product_name }}</p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <form method="GET" action="/products/{{ $product->id }}">
-                                                @csrf
-                                                <button type="submit" class="btn btn-sm btn-outline-primary">Order
-                                                    Item</button>
-                                            </form>
-                                        </div>
-                                        <small class="text-body-secondary">₱ {{ $product->price }}.00</small>
-                                    </div>
-                                </div>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-3">
+            @forelse($products as $product)
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="{{ asset('storage/' . $product->image_path) }}" alt="" width="100%"
+                            height="225" data-bs-toggle="modal" data-bs-target="#myProduct{{ $product->id }}">
+                        <div class="card-body">
+                            <p class="card-text">{{ $product->product_name }}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <form method="GET" action="/products/{{ $product->id }}">
+                                    @csrf
+                                    <button class="btn btn-sm btn-outline-primary">Order Item</button>
+                                </form>
+                                <small class="text-body-secondary">P{{ $product->price }}.00</small>
                             </div>
                         </div>
-                        <!--End of col-->
-                        <!-- Modal -->
-                        <div class="modal fade" id="myProduct{{ $product->id }}" data-bs-backdrop="static"
-                            data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="myProduct{{ $product->id }}">
-                                            {{ $product->product_name }}</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <img src="{{ asset('storage/' . $product->image_path) }}" alt=""
-                                            width="100%" height="300">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @empty
-                        <p>No Products Yet!</p>
-                    @endforelse
+                    </div>
                 </div>
-            </div>
+                {{-- end of col --}}
+                {{-- modal --}}
+                <div class="modal fade" id="myProduct{{ $product->id }}">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5">{{ $product->product_name }}</h1>
+                                <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="{{ asset('storage/' . $product->image_path) }}" alt="" width="100%"
+                                    height="300">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <p>No Products Available.</p>
+            @endforelse
         </div>
-
-    </main>
+    </div>
 </body>
 
 </html>
--- laravel-app/resources/views/product/order.blade.php
+++ laravel-app/resources/views/product/order.blade.php
@@ -11,84 +11,90 @@
 
 <body class="bg-dark-subtle">
     <div class="container py-4">
-        <header class="pb-3 mb-4 border-bottom">
-            <a href="/" class="d-flex align-items-center text-decoration-none">
+        <header class="mb-4 border-bottom">
+            <a href="/" class="text-decoration-none">
                 <span class="fs-4">back</span>
             </a>
         </header>
 
-
-        <div class="row align-items-md-stretch">
-            <div class="col-md-6">
+        <div class="row align-items-stretch p-md-5">
+            <div class="col-6">
                 <img class="bd-placeholder-img card-img-top" width="50%" height="400"
                     src="{{ asset('storage/' . $product->image_path) }}" alt="{{ $product->product_name }}">
             </div>
-            <div class="col-md-6">
-                <form method="POST" action="/orders">
+            <div class="col-6 mt-4 mt-md-0">
+                <form method="POST" action="/orders" onsubmit="return confirmSubmit()">
                     @csrf
-                    <div class="h-100 p-5 bg-body-tertiary border rounded-3">
-                        <div class="p-5">
-                            <input type="hidden" name="product_id" value="{{ $product->id }}">
-                            <div class="me-2 mb-3 d-flex justify-content-between">
-                                <h3 class="fs-4"> {{ $product->product_name }} </h3>
-                                <span class="text-body-secondary" id="totalPrice"> ₱
-                                    {{ $product->price }}.00 </span>
-                                <input type="hidden" id="price" name="price" value="{{ $product->price }}">
-                            </div>
-                            <div class="input-group mb-2 border border-dark-subtle rounded-3">
-                                <span class="input-group-text bg-dark-subtle">
-                                    Qty</span>
-                                <input type="number" name="quantity" id="quantity" value="1" min="1"
-                                    class="form-control">
-                                <!-- Error Message -->
-                                @error('quantity')
-                                    <span class="text-danger lead mb-1" role="alert">
-                                        <small>{{ $message }}</small>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="input-group mb-2 border border-dark-subtle rounded-3">
-                                <span class="input-group-text bg-dark-subtle">
-                                    Name of Buyer:</span>
-                                <input type="text" class="form-control" name="buyer_name">
-                            </div>
+                    <div class="h-100 p-3 p-md-5 bg-body-secondary border rounded-3">
+                        <input type="hidden" name="product_id" value="{{ $product->id }}">
+                        <div class="d-flex justify-content-between">
+                            <h3 class="fs-5">{{ $product->product_name }}</h3>
+                            <span class="text-md-body-secondary" id="totalPrice"> ₱
+                                {{ $product->price }}.00 </span>
+                            <input type="hidden" id="price" name="price" value="{{ $product->price }}">
+                        </div>
+                        <div class="input-group mb-2 mt-4 border rounded-3 border-dark-subtle">
+                            <span class="input-group-text bg-dark-subtle">
+                                Qty</span>
+                            <input type="number" name="quantity" id="quantity" value="1" min="1"
+                                class="form-control">
                             <!-- Error Message -->
-                            @error('buyer_name')
+                            @error('quantity')
                                 <span class="text-danger lead mb-1" role="alert">
                                     <small>{{ $message }}</small>
                                 </span>
                             @enderror
-                            <div class="input-group mb-2 border border-dark-subtle rounded-3">
-                                <span class="input-group-text bg-dark-subtle">
-                                    Contact Number: </span>
-                                <input type="tel" class="form-control" name="mobile_number" maxlength="11"
-                                    oninput="this.value = this.value.replace(/[^0-9]/g, '')">
-                            </div>
-                            <!-- Error Message -->
-                            @error('mobile_number')
-                                <span class="text-danger lead mb-1" role="alert">
-                                    <small>{{ $message }}</small>
-                                </span>
-                            @enderror
-                            <div class="d-flex justify-content-end mb-3">
-                                <button class="btn btn-outline-primary" type="submit">Submit</button>
-                            </div>
-                            <!-- Alert Success -->
-                            @if (session('success'))
+                        </div>
+                        <div class="input-group mb-2 border border-dark-subtle rounded-3">
+                            <span class="input-group-text bg-dark-subtle">
+                                Name of Buyer:</span>
+                            <input type="text" class="form-control" name="buyer_name">
+                        </div>
+                        <!-- Error Message -->
+                        @error('buyer_name')
+                            <span class="text-danger lead mb-1" role="alert">
+                                <small>{{ $message }}</small>
+                            </span>
+                        @enderror
+                        <div class="input-group mb-2 border border-dark-subtle rounded-3">
+                            <span class="input-group-text bg-dark-subtle">
+                                Contact Number: </span>
+                            <input type="tel" class="form-control" name="mobile_number" maxlength="11"
+                                oninput="this.value = this.value.replace(/[^0-9]/g, '')">
+                        </div>
+                        <!-- Error Message -->
+                        @error('mobile_number')
+                            <span class="text-danger lead mb-1" role="alert">
+                                <small>{{ $message }}</small>
+                            </span>
+                        @enderror
+                        <div class="d-flex justify-content-end mt-4">
+                            <button class="btn btn-outline-primary" type="submit">Submit</button>
+                        </div>
+                        <!-- Alert Success -->
+                        {{-- @if (session('success'))
                                 <div class="alert alert-success">
                                     {{ session('success') }}
                                 </div>
-                            @endif
-                        </div>
+                            @endif --}}
                     </div>
                 </form>
             </div>
         </div>
-        <footer class="pt-3 mt-4 text-body-secondary border-top">
+        <footer class="mt-4 pt-4 border-top">
             © Crisbel Enobay
         </footer>
     </div>
-
+    <!-- Bootstrap Modal -->
+    <div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="successModalLabel"
+        aria-hidden="true">
+        <div class="container">
+            <div class="alert alert-success mt-2 d-flex justify-content-between"><span>Order sent to email.</span>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                </button>
+            </div>
+        </div>
+    </div>
     {{-- <script>
         document.getElementById('quantity').addEventListener('input', function() {
             const quantity = parseInt(this.value, 10);
@@ -107,6 +113,34 @@
         });
     </script> --}}
     <script>
+        function confirmSubmit() {
+            // Validate buyer name and contact number
+            var buyerName = document.getElementsByName('buyer_name')[0].value;
+            var contactNumber = document.getElementsByName('mobile_number')[0].value;
+
+            if (buyerName.trim() === '' || contactNumber.trim() === '') {
+                // Show error message
+                alert('Buyer name and contact number are required.');
+                return false; // Prevent form submission
+            }
+            // Display a confirmation modal
+            if (confirm('Are you sure you want to submit this order?')) {
+                // Show the Bootstrap modal
+                var successModal = document.getElementById('successModal');
+                successModal.classList.add('show');
+                successModal.style.display = 'block';
+
+                // Hide the modal after 3 seconds
+                setTimeout(() => {
+                    successModal.classList.remove('show');
+                    successModal.style.display = 'none';
+                }, 3000);
+
+                return true; // Allow form submission
+            } else {
+                return false; // Prevent form submission
+            }
+        }
         document.getElementById('quantity').addEventListener('input', function() {
             const quantity = parseInt(this.value, 10);
             const price = parseFloat({{ $product->price }});
@@ -118,14 +152,14 @@
             document.getElementById('price').value = totalPrice.toFixed(2);
         });
     </script>
-    <script>
+    {{-- <script>
         var successAlert = document.querySelector('.alert-success');
         if (successAlert) {
             setTimeout(() => {
                 successAlert.style.display = "none";
             }, 3000);
         }
-    </script>
+    </script> --}}
 
 
 </body>
